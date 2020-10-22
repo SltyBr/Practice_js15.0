931236d917ff2263d2e9d37ac90bc37a4c861e17
SaltyBr <SaltyBrain@yandex.ru>
2020-10-22 17:19:25 +0300

    practice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const collections = document.querySelectorAll('.collection'),
-      elems = document.querySelectorAll('.elem');
+      elems = document.querySelectorAll('.elem'),
+      secondHead = document.getElementById('second-head');
 
 const newElem = document.createElement('li'); // создаёт парный тэг html, можно указать любую комбинацию
+
+newElem.textContent = 'Новый элемент';
+newElem.classList.add('new_elem');
 
 console.log(newElem);
 
@@ -15,6 +19,7 @@ console.log(elems);
 
 /* collections[1].append(elems[3]);  */// вставляем элемент в конец родителя DOM дерева
 /* collections[1].append(elems[1]); */ // именно берёт и перемещает элементы, не копирует
+/* collections[1].prepend(elem[5]) */ // вставляет в начале родителя элемент
 
 /* collections[0].before(collections[1]); */ // берет аргумент коллекции1 и вставляет перед коллецкией0
 
@@ -31,3 +36,8 @@ elemClone.textContent = 'new elem'; */
 /* collections[1].append(elemClone); */
 
 elems[2].textContent = 'Привет'; // переписывает текст внутри тэга
+collections[1].append(newElem);
+
+secondHead.insertAdjacentText('afterbegin', 'beforebegin'); // вставляет текст до/внутри/после тэга
+secondHead.insertAdjacentElement('beforebegin', elems[4]); // вставляет элемент, текст не принимает, до/внутри/после тэга
+secondHead.insertAdjacentHTML('afterend', '<h3>Привет мир</h3>'); // вставляет элемент, текст не принимает, до/внутри/после тэга
