3709c4f4cdc0baee9a0a95f29f914db983e933fb
SaltyBr <SaltyBrain@yandex.ru>
2020-10-28 14:27:48 +0300

    almost done

--- script.js
+++ script.js
@@ -3,68 +3,34 @@
 const randomColor = document.querySelector('.random-color'),
     body = document.body,
     getRandom = document.querySelector('.get-random');
-/* у this есть четыре правила  callstack и call site*/
 
-function one(){
-    console.log('one');
-    two();
-}
-function two(){
-    console.log('two');
-    three();
-}
-function three(){
-    console.log('three');
-}
+body.style.backgroundColor = randomColor.textContent;
 
-one(); // место вызова функции call site
+const getRandomColor = function(){
+    let num1 = parseInt(Math.random()*255).toString(16),
+        num2 = parseInt(Math.random()*255).toString(16),
+        num3 = parseInt(Math.random()*255).toString(16);
+    
+    let num = num1 + num2 + num3;
+
+    if (num1 == 0){
+        num1 = 0 + '0';
+    }
+    if (num2 == 0){
+        num2 = 0 + '0';
+    }
+    if (num3 == 0){
+        num3 = 0 + '0';
+    }
+    let hex = '#' + num;
 
 
-function test(){
-    console.log('hello', this);
-}
-//1. привязка this по умолчанию foo()
-test();
-
-//2. неявная привязка, это когда указываем объект и его метод obj.foo()
-
-//3. явная привязка apply, call, bind
-
-let obj = {
-    x: 10,
-    y: 15
+        body.style.backgroundColor = hex;
+        getRandom.style.color = hex;
+        randomColor.textContent = hex;
+    
+    console.log(num);
 };
 
-function newTest(){
-    console.log('this: ', this);
-}
+getRandom.addEventListener('click', getRandomColor);
 
-function hardBind(hard){
-    newTest.call(hard);
-}
-hardBind(obj); // жесткая привязка
-
-setTimeout(hardBind, 5000, obj);
-
-newTest.apply(obj); // принимает массив аргументов
-newTest.call(obj); // принимает сколько угодно параметров через запятую
-
-let foo = newTest.bind(obj);
-
-foo();
-
-//============================
-
-// 4. привязка new
-
-randomColor.style.color = '#FFFFFF';
-body.style.backgroundColor = randomColor.textContent; 
-
-getRandom.addEventListener('click', function(){
-    let num = parseInt(Math.random()*255).toString(16)+parseInt(Math.random()*255).toString(16)+parseInt(Math.random()*255).toString(16),
-        hex = '#' + num;
-
-    body.style.backgroundColor = hex;
-    getRandom.style.color = hex;
-    randomColor.textContent = hex;
-});
