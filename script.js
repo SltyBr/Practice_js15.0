bb5e0a790a1b964aaf3b82f8f83775be457f5834
SaltyBr <SaltyBrain@yandex.ru>
2020-10-28 12:21:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,36 @@ one(); // место вызова функции call site
 function test(){
     console.log('hello', this);
 }
-//привязка this по умолчанию
+//1. привязка this по умолчанию foo()
 test();
 
+//2. неявная привязка, это когда указываем объект и его метод obj.foo()
+
+//3. явная привязка apply, call, bind
+
+let obj = {
+    x: 10,
+    y: 15
+};
+
+function newTest(){
+    console.log('this: ', this);
+}
+
+function hardBind(hard){
+    newTest.call(hard);
+}
+hardBind(obj); // жесткая привязка
+
+setTimeout(hardBind, 5000, obj);
+
+newTest.apply(obj); // принимает массив аргументов
+newTest.call(obj); // принимает сколько угодно параметров через запятую
+
+let foo = newTest.bind(obj);
+
+foo();
+
+//============================
+
+// 4. привязка new
