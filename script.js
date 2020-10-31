9aef711b07c6d58fee99e8d8ff5e1783f2b83b3b
SaltyBr <SaltyBrain@yandex.ru>
2020-10-31 12:55:22 +0300

    lesson14_1 begin

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const container = document.querySelector('.container'),
+      createElement = document.createElement('li');
+
+console.log(createElement);
+
+createElement.textContent = 'Привет';
+
+container.append(createElement);
