f9dbbf61e921e9d2d163f662fd1434d00131e834
SaltyBr <SaltyBrain@yandex.ru>
2020-10-31 14:45:26 +0300

    lesson14_1 task done

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 'use strict';
 
-const container = document.querySelector('.container'),
-      createElement = document.createElement('li');
+const DomElement = function(selector, height, width, bg, fontSize){ //создаём класс с DomElement
+  this.selector =  selector;
+  this.height =  height;
+  this.width =  width;
+  this.bg =  bg;
+  this.fontSize = fontSize;
+};
 
-console.log(createElement);
+const box = new DomElement('#weqwew', '100', '100', 'red', '20'); //определяем экземпляр класса, вводим аргументы
 
-createElement.textContent = 'Привет';
+DomElement.prototype.createEl = function(){ // добавляем в proto метод, который будем оперировать аргументами
+  let el;
+  if (this.selector[0] === '.'){ //случай, когда первый элемент точка
+      this.selector = this.selector.slice(1); // получаем имя селектора
+      el = document.createElement('div');
+      el.className = this.selector;
+      el.innerText = 'Это класс';
+      document.body.prepend(el); // добавляем элемент в DOM структуру
+  } else if (this.selector[0] === '#'){ //случай, когда первый элемент решетка
+    this.selector = this.selector.slice(1);
+    el = document.createElement('p');
+      el.id = this.selector;
+      el.innerText = 'Это айди';
+      document.body.prepend(el);
+  } else { // просим ввести корректный селектор
+    document.body.innerHTML = 'Селектор должен начинаться с . или #';}
+    // с помощью cssText добавляем css свойства элементу
+    el.style.cssText = `
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: red;
+      font-size: ${this.fontSize}px;
+    `;
+};
 
-container.append(createElement);
+box.createEl(); //объявляем метод, любуемся
+
+console.log(box);
