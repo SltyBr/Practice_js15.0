4fae0c007fe050d55ceedae0d8a15c11709bf78a
SaltyBr <SaltyBrain@yandex.ru>
2020-11-01 14:21:50 +0300

    update script

--- script.js
+++ script.js
@@ -59,3 +59,76 @@ car.fullTitle = 'BMW';
 console.log(car.fullTitle);
 
 console.log(car.fullTitle);
+
+class CarWash {
+    constructor(brand, model = CarWash.noCarBaseModel(), services = []){ // метод который создаётся в момент создания класса и подготавливает объект
+        this.brand = brand;
+        this.model = model;
+        this.washed = false;
+        this._services = services;
+    }
+
+    static noCarBaseModel(){ // статический метод, возвращает значение по умолчанию, нельзя вызвать из объекта
+        return 'none';
+    }
+
+    washReady(){
+        this.washed = true;
+        CarWash.counter++;
+        this.report();
+    }
+
+    report(){
+        console.log(this.brand, this.model, this.washed);
+    }
+
+    get services(){
+        console.log(this._services);
+        return this._services.length > 0 ? 'Есть доп услуги' : 'Нет доп услуг';
+    }
+
+    set services(addServices){
+        return this._services.push(addServices);
+    }
+}
+
+// наследование, такая запись позволяет passcar наследовать всё от carwash, для наследования определённых свойств используем super
+class PassCar extends CarWash{
+    constructor(brand, model, services, pass = 5){ // наследуем конструктор от родителя, можем расширять
+        super(brand, model, services);
+        this.pass = pass;
+    }
+
+    washReady(){
+        super.washReady(); // можно наследовать метод через super с указанием имени метода
+        this.reportOffice();
+    }
+
+    reportOffice(){
+        console.log('На мойке для легковых была помыта машина');
+    }
+}
+
+CarWash.counter = 0;
+
+const car1 = new CarWash('mazda', 3, ['black tires', 'wax']);
+const car2 = new PassCar('BMW', 2);
+
+
+car1.washReady();
+
+console.log('counter: ' + CarWash.counter);
+
+
+console.log('counter: ' + CarWash.counter);
+
+car1.services = 'Протирка стёкол';
+console.log(car1.services);
+console.log(car2.services);
+
+
+
+console.log('машина 1 ', car1);
+console.log('машина 2 ', car2);
+
+car2.washReady();
