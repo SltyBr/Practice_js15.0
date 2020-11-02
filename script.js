61391b1cb1784e678a737791922e717811f49744
SaltyBr <SaltyBrain@yandex.ru>
2020-11-02 13:45:36 +0300

    123123

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
 'use strict';
 
-const myLesson = ([
-    {
-        lesson: 1, 
-        type: 'basic', 
-        points: 2
-    },
-    {
-        lesson: 2, 
-        type: 'additional', 
-        points: 4
-    },
-    {
-        lesson: 3, 
-        type: 'basic', 
-        points: 6},
-    {
-        lesson: 4, 
-        type: 'additional', 
-        points: 3},
-    {
-        lesson: 5, 
-        type: 'basic', 
-        points: 4},
-    {
-        lesson: 6, 
-        type: 'basic', 
-        points: 2
-    },
-    {
-        lesson: 7, 
-        type: 'additional', 
-        points: 2
-    },
-    {
-        lesson: 8, 
-        type: 'basic', 
-        points: 6
-    },
-    {
-        lesson: 9, 
-        type: 'basic', 
-        points: 4
-    }
-]);
+const myLesson = [
+    {lesson: 1, type: 'basic', points: 2},
+    {lesson: 2, type: 'additional', points: 4},
+    {lesson: 3, type: 'basic', points: 6},
+    {lesson: 4, type: 'additional', points: 3},
+    {lesson: 5, type: 'basic', points: 4},
+    {lesson: 6, type: 'basic', points: 2},
+    {lesson: 7, type: 'additional', points: 2},
+    {lesson: 8, type: 'basic', points: 6},
+    {lesson: 9, type: 'basic', points: 4},
+    {lesson: 10, type: 'basic', points: 6},
+    {lesson: 11, type: 'additional', points: 5}, 
+    {lesson: 12, type: 'basic', points: 2}, 
+    {lesson: 13, type: 'additional', points: 2}, 
+    {lesson: 14, type: 'basic', points: 4},
+    {lesson: 15, type: 'additional', points: 1},
+    {lesson: 16, type: 'additional', points: 7},
+  ];
 
-const lessonMap = new Set();
+const myLessonEach = (arr) =>{
+    console.log(arr);
+};
 
-myLesson.forEach( (key)=>{
-    console.log(key);
-});
-
-console.log(myLesson[3]);
+myLessonEach(myLesson);
