e2422fffc9e8a3fdfb360de48c5fa8facd07fa7d
SaltyBr <SaltyBrain@yandex.ru>
2020-11-03 17:01:45 +0300

    add task done

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 'use strict';
 
 const myLesson = [
-    {lesson: 1, type: 'basic', points: 2},
-    {lesson: 2, type: 'additional', points: 4},
-    {lesson: 3, type: 'basic', points: 6},
-    {lesson: 4, type: 'additional', points: 3},
-    {lesson: 5, type: 'basic', points: 4},
-    {lesson: 6, type: 'basic', points: 2},
-    {lesson: 7, type: 'additional', points: 2},
-    {lesson: 8, type: 'basic', points: 6},
-    {lesson: 9, type: 'basic', points: 4},
-    {lesson: 10, type: 'basic', points: 6},
-    {lesson: 11, type: 'additional', points: 5}, 
-    {lesson: 12, type: 'basic', points: 2}, 
-    {lesson: 13, type: 'additional', points: 2}, 
-    {lesson: 14, type: 'basic', points: 4},
-    {lesson: 15, type: 'additional', points: 1},
-    {lesson: 16, type: 'additional', points: 7},
-  ];
+  {lesson: 1, type: 'basic', points: 2},
+  {lesson: 2, type: 'additional', points: 4},
+  {lesson: 3, type: 'basic', points: 6},
+  {lesson: 4, type: 'additional', points: 3},
+  {lesson: 5, type: 'basic', points: 4},
+  {lesson: 6, type: 'basic', points: 2},
+  {lesson: 7, type: 'additional', points: 2},
+  {lesson: 8, type: 'basic', points: 6},
+  {lesson: 9, type: 'basic', points: 4},
+  {lesson: 10, type: 'basic', points: 6},
+  {lesson: 11, type: 'additional', points: 5}, 
+  {lesson: 12, type: 'basic', points: 2}, 
+  {lesson: 13, type: 'additional', points: 2}, 
+  {lesson: 14, type: 'basic', points: 4},
+  {lesson: 15, type: 'additional', points: 1},
+  {lesson: 16, type: 'additional', points: 7},
+],
+    finalLesson = [],
+    myLesson1 = new Map(Object.entries(myLesson));
 
+console.log('Старый массив: ', myLesson);
 
-myLesson.forEach(el => {
-    el.has('basic');
-});
+for (let entries of myLesson1.entries()){ // удаляем ключи, где есть свойство со значением additional
+  if (entries[1].type === 'additional'){
+    myLesson1.delete(entries[0]);
+  }
+}
+
+for (let values of myLesson1.values()){ // получаем свойства, делим  points на два
+  values.points = values.points/2;
+  myLesson.push(values); //  пушим всё в новый массив
+}
+
+console.log('Новый массив: ', finalLesson);
